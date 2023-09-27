ae147e92f33da613a84c41ef0a9cd4ee851b1d72
kingoffier <144258616+kingoffier@users.noreply.github.com>
2023-09-27 21:26:42 +0500

    Add files via upload

---
+++ Homework 2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите координаты точки");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите переменную m - ");
+            double m = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите переменную n - ");
+            double n = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(x>=m & x<=n ? "Точка принадлежит закрашенной области":"Точка не принадлежит закрашенной области");
+            Console.ReadKey();
+        }
+    }
+}
