cc4d7105e9bfffa847b9ad29dd29100796ed0729
kingoffier <144258616+kingoffier@users.noreply.github.com>
2023-09-27 21:22:08 +0500

    Add files via upload

---
+++ Homework If.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите координаты точки");
+            double x=Convert.ToDouble(Console.ReadLine());
+            double y=Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите переменную m - ");
+            double m=Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите переменную n - ");
+            double n =Convert.ToDouble(Console.ReadLine());
+            if (x>=m & x<=n)
+            {
+                Console.WriteLine("Точка принадлежит чисти плоскости");
+            }
+            else
+            {
+                Console.WriteLine("Точка не принадлежит части плоскости");
+            }
+            Console.ReadKey();
+        }
+    }
+}
